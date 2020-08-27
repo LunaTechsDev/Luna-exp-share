851a0dd410cb55afeb767e67a37bd1c21e372324
Kino  Rose <kinoarose@gmail.com>
2020-08-26 20:37:11 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# LunarBase
+# Luna-Exp-Share
+> Allows you to share experience with your benched characters in RPGMakerMV/MZ
---
+++ src/LunaExpShare.hx
@@ -0,0 +1,9 @@
+import macros.MacroTools;
+import rm.managers.PluginManager;
+import utils.Fn;
+import utils.Comment;
+
+class LunaExpShare {
+ public static function main() {
+ }
+}
