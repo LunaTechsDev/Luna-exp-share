1d99280d6ef24161cec5eb43d28fc151dd148d14
Kino  Rose <kinoarose@gmail.com>
2020-08-26 21:02:40 -0400

    Complete Exp Share Plugin

--- src/LunaExpShare.hx
+++ src/LunaExpShare.hx
@@ -1,9 +1,27 @@
-import macros.MacroTools;
+import core.Types.JsFn;
+import rm.objects.Game_Actor;
+import rm.Globals;
 import rm.managers.PluginManager;
 import utils.Fn;
 import utils.Comment;
 
 class LunaExpShare {
  public static function main() {
+  final params = Globals.Plugins.filter((plugin) -> {
+   return ~/<LunaExpShare>/ig.match(plugin.description);
+  })[0].parameters;
+
+  final ExpShareParam = Fn.parseFloatJs(params["ReserveExp"]);
+
+  Comment.title("Game_Actor");
+  var oldBenchExpRate: JsFn = Fn.getPrProp(Game_Actor, "benchMembersExpRate");
+  Fn.setPrProp(Game_Actor, "benchMembersExpRate", () -> {
+   return
+    Globals.DataSystem.optExtraExp ? percentToFloat(ExpShareParam) : oldBenchExpRate.call(Fn.self);
+  });
+ }
+
+ public static inline function percentToFloat(percent: Float) {
+  return percent / 100.0;
  }
 }
